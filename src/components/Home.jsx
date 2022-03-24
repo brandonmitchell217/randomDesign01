83d3da2da94bea92a0a2733079aa9f2c2644b0cb
Brandon Mitchell <brandonmitchell217@gmail.com>
2022-03-23 23:42:06 -0400

    Added services to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,39 +5,42 @@ import netflix from '../assets/images/netflix.png'
 import microsoft from '../assets/images/microsoft.png'
 import google from '../assets/images/google.png'
 import spotify from '../assets/images/spotify.png'
-import '../index.css'
+import HomeServices from './pageComps/HomeServices'
 
 const Home = () => {
   return (
-    <section className="home">
-      <div className="cover">
-        <div className="topLeft">
-          <h1>Lorem Ipsum Dolor</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
-            ex ratione neque? Ullam, quaerat!
-          </p>
-          <div className="ctas">
-            <Link to="/" className="cta">
-              Lorem Ipsum
-            </Link>
-            <Link to="/" className="cta">
-              Lorem Ipsum
-            </Link>
+    <>
+      <section className="home">
+        <div className="cover">
+          <div className="topLeft">
+            <h1>Lorem Ipsum Dolor</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+              ex ratione neque? Ullam, quaerat!
+            </p>
+            <div className="ctas">
+              <Link to="/" className="cta">
+                Lorem Ipsum
+              </Link>
+              <Link to="/" className="cta">
+                Lorem Ipsum
+              </Link>
+            </div>
+          </div>
+          <div className="bottomRight">
+            <img src={landingComp} alt="Computer" className="comp1" />
+            <img src={landingPhone} alt="Phone" className="phone1" />
           </div>
         </div>
-        <div className="bottomRight">
-          <img src={landingComp} alt="Computer" className="comp1" />
-          <img src={landingPhone} alt="Phone" className="phone1" />
+        <div className="companies">
+          <img src={netflix} alt="Netflix" />
+          <img src={microsoft} alt="Microsoft" />
+          <img src={google} alt="Google" />
+          <img src={spotify} alt="Spotify" />
         </div>
-      </div>
-      <div className="companies">
-        <img src={netflix} alt="" />
-        <img src={microsoft} alt="" />
-        <img src={google} alt="" />
-        <img src={spotify} alt="" />
-      </div>
-    </section>
+      </section>
+      <HomeServices />
+    </>
   )
 }
 
---
+++ src/components/pageComps/HomeServices.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const HomeServices = () => {
+  return (
+    <section className="homeServices">
+      <header>
+        <h2>Lorem Ipsum Dolor Sit</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+          praesentium ea blanditiis.
+        </p>
+        <div className="ctas">
+          <Link to="/" className="cta">
+            Lorem Ipsum
+          </Link>
+          <Link to="/" className="cta">
+            Lorem Ipsum
+          </Link>
+        </div>
+      </header>
+      <div className="service">
+        <h3>Basic</h3>
+        <span>$19.99/Mo</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
+          libero quae omnis.
+        </p>
+        <Link to="/" className="serviceLink">
+          Lorem Ipsum
+        </Link>
+      </div>
+      <div className="service">
+        <h3>Plus</h3>
+        <span>$39.99/Mo</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
+          libero quae omnis.
+        </p>
+        <Link to="/" className="serviceLink">
+          Lorem Ipsum
+        </Link>
+      </div>
+      <div className="service">
+        <h3>Premium</h3>
+        <span>$49.99/Mo</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
+          libero quae omnis.
+        </p>
+        <Link to="/" className="serviceLink">
+          Lorem Ipsum
+        </Link>
+      </div>
+    </section>
+  )
+}
+
+export default HomeServices
